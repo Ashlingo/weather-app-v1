f7531b34fe1121f7791a79fdaedbf4288f1c8e89
Ashlingo <otooleashling@gmail.com>
2023-12-11 21:50:35 +0000

    updated with humidity element

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ function displayTemperature(response) {
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElemnt = document.querySelector("#humidity");
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = temperature;
   descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 }
 
 function search(event) {
